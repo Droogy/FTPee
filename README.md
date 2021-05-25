41d6437c6990b7c44f698363b11c8f156b15a4fc
Droogy <drooogy@pm.me>
2021-05-25 14:28:03 -0400

    Adding new code to repo

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # FTPee
 A CTF Tool that you can use to check if a FTP Server is running and get any of the files mentioned in the list.
+
+## Usage
+
+Simply make FTPee executable and supply the target as a command line argument.
+`./FTPee 10.0.0.1`
+
+## To-Do
+- Automatically check for directories and grab files
+- General error-handling
+- Output which files were found and downloaded
--- ftp.sh
+++ ftp.sh
@@ -1,80 +1,42 @@
 #!/bin/bash
 
-
-
 ###########################
 #                         #
-#Evann2007 on discord.    #
+# Discord: Evann2007      #
 #                         #
 #                         # 
-#     FTPee a Hacking Tool.#
-#                           #
-#                           #
-########################## #
+# FTPee - a Hacking Tool  #
+#                         #
+#                         #
+###########################
 
 #below is our host, the initial ftp server that we wanna access, when you run this script how i have it it will NOT work.
 
 #you need to replace our HOST= with the ip/ftp server that you're trying to access
 
+# host is our first command line argument to create dynamic variable
+# e.g. "./FTPee 10.10.10.10."
 
-HOST='10.10.10.10'
-
-
-echo thank you for entering the host.
-
+HOST=$1
 USER='Anonymous'
+PASS=' '
+# store output of netcat banner grab in a variable
+banner=$(echo "quit" | nc -v ${HOST} 21 2>&1)
+echo "[*] Grabbing FTP banner"
 
-time sleep 2
+# check for 220 FTP response code in our banner
+if [[ "$banner" == *"220"* ]]; then
+    echo "[*] FTP Banner was found, grabbing files"
+    ftp -v -n >> /tmp/ftp.log << EOF
+    open $HOST
+    user $USER $PASS
+    binary
+    mget *
+    quit
+EOF
+else
+    echo "[X] No FTP Server running on port 21 for $HOST"
+fi
 
-echo thank you for entering your user.
-
-PASSWD='Anonymous'
-
-time sleep 2
-
-echo thank you for entering your password.
-
-ftp -n -v $HOST << EOT
-
-ascii
-
-
-ls -al
-
-
-
-#now we are going to list all of the names of directorys/files that we want.
-
-get note
-
-get user_flag.txt
-
-get root_flag.txt
-
-get flag.txt
-
-get user.txt
-
-get note.txt
-
-get root.txt
-
-get root
-
-get user
-
-
-get flag
-
-
-#then we exit our server because there is nothing else we need.
-
-bye
-
-echo  bye thank's for using ftpee!
-
-time sleep 2
-
-
-EOT
+echo 'Bye thanks for using FTPee!'
 
